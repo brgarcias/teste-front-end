698f6ee78e0c957e5e818ef80c890b7606b32ad9
brgarcias <bruno-151299@hotmail.com>
2021-02-10 21:20:56 -0300

    Publicando projeto

---
+++ js/main.js
@@ -0,0 +1,196 @@
+(function () {
+
+	(function () {
+		let produtosJson = '';
+		let produtosModais = "";
+		var flickerAPI = "json/produto.json";
+		$.getJSON(flickerAPI, {
+			tags: "mount rainier",
+			tagmode: "any",
+			format: "json"
+		})
+			.done(function (data) {
+				for (i = 0; i < data.products.length; i++) {
+					produtosJson += '<div class="product"><div class="product__info">';
+					produtosJson += '<img class="product__image" src="' + data.products[i].photo + '" alt="Product 1" />';
+					produtosJson += '<h3 class="product__title">' + data.products[i].productName + '</h3>';
+					produtosJson += '<span class="product__varietal extra highlight">' + data.products[i].descriptionShort + '</span>';
+					produtosJson += '<span class="product__price highlight">R$ ' + data.products[i].price + '</span>';
+					produtosJson += '<button data-izimodal-open="#modal' + i + '" data-izimodal-transitionin="fadeInDown" class="trigger action action--button action--buy"><i class="fa fa-search"></i><span class="action__text">Ver detalhes</span></button></div><label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Clique para comparar</span></label></div>';
+				}
+				$('#resultado').html(produtosJson);
+				for (i = 0; i < data.products.length; i++) {
+					produtosModais = ' ';
+					produtosModais += '<div class="product"><div class="product__info">';
+					produtosModais += '<img class="product__image" src="' + data.products[i].photo + '" alt="Produto ' + i + 1 +'" />';
+					produtosModais += '<h3 class="product__title">' + data.products[i].productName + '</h3>';
+					produtosModais += '<span class="product__varietal extra highlight">' + data.products[i].descriptionShort + '</span>';
+					produtosModais += '<span class="product__price highlight">R$ ' + data.products[i].price + '</span>';
+					$("#modal" + i).iziModal();
+					$("#modal" + i).html(produtosModais);
+				}
+			});
+	})();
+
+	var viewEl = document.querySelector('.view'),
+		gridEl = viewEl.querySelector('.grid'),
+		items = [].slice.call(gridEl.querySelectorAll('.product')),
+		basket;
+
+	// the compare basket
+	function CompareBasket() {
+		this.el = document.querySelector('.compare-basket');
+		this.compareCtrl = this.el.querySelector('.action--compare');
+		this.compareWrapper = document.querySelector('.compare'),
+			this.closeCompareCtrl = this.compareWrapper.querySelector('.action--close')
+
+		this.itemsAllowed = 3;
+		this.totalItems = 0;
+		this.items = [];
+
+		// compares items in the compare basket: opens the compare products wrapper
+		this.compareCtrl.addEventListener('click', this._compareItems.bind(this));
+		// close the compare products wrapper
+		var self = this;
+		this.closeCompareCtrl.addEventListener('click', function () {
+			// toggle compare basket
+			classie.add(self.el, 'compare-basket--active');
+			// animate..
+			classie.remove(viewEl, 'view--compare');
+		});
+	}
+
+	CompareBasket.prototype.add = function (item) {
+		// check limit
+		if (this.isFull()) {
+			return false;
+		}
+
+		classie.add(item, 'product--selected');
+
+		// create item preview element
+		var preview = this._createItemPreview(item);
+		// prepend it to the basket
+		this.el.insertBefore(preview, this.el.childNodes[0]);
+		// insert item
+		this.items.push(preview);
+
+		this.totalItems++;
+		if (this.isFull()) {
+			classie.add(this.el, 'compare-basket--full');
+		}
+
+		classie.add(this.el, 'compare-basket--active');
+	};
+
+	CompareBasket.prototype._createItemPreview = function (item) {
+		var self = this;
+
+		var preview = document.createElement('div');
+		preview.className = 'product-icon';
+		preview.setAttribute('data-idx', items.indexOf(item));
+
+		var removeCtrl = document.createElement('button');
+		removeCtrl.className = 'action action--remove';
+		removeCtrl.innerHTML = '<i class="fa fa-remove"></i><span class="action__text action__text--invisible">Remove product</span>';
+		removeCtrl.addEventListener('click', function () {
+			self.remove(item);
+		});
+
+		var productImageEl = item.querySelector('img.product__image').cloneNode(true);
+
+		preview.appendChild(productImageEl);
+		preview.appendChild(removeCtrl);
+
+		var productInfo = item.querySelector('.product__info').innerHTML;
+		preview.setAttribute('data-info', productInfo);
+
+		return preview;
+	};
+
+	CompareBasket.prototype.remove = function (item) {
+		classie.remove(this.el, 'compare-basket--full');
+		classie.remove(item, 'product--selected');
+		var preview = this.el.querySelector('[data-idx = "' + items.indexOf(item) + '"]');
+		this.el.removeChild(preview);
+		this.totalItems--;
+
+		var indexRemove = this.items.indexOf(preview);
+		this.items.splice(indexRemove, 1);
+
+		if (this.totalItems === 0) {
+			classie.remove(this.el, 'compare-basket--active');
+		}
+
+		// checkbox
+		var checkbox = item.querySelector('.action--compare-add > input[type = "checkbox"]');
+		if (checkbox.checked) {
+			checkbox.checked = false;
+		}
+	};
+
+	CompareBasket.prototype._compareItems = function () {
+		var self = this;
+
+		// remove all previous items inside the compareWrapper element
+		[].slice.call(this.compareWrapper.querySelectorAll('div.compare__item')).forEach(function (item) {
+			self.compareWrapper.removeChild(item);
+		});
+
+		for (var i = 0; i < this.totalItems; ++i) {
+			var compareItemWrapper = document.createElement('div');
+			compareItemWrapper.className = 'compare__item';
+
+			var compareItemEffectEl = document.createElement('div');
+			compareItemEffectEl.className = 'compare__effect';
+
+			compareItemEffectEl.innerHTML = this.items[i].getAttribute('data-info');
+			compareItemWrapper.appendChild(compareItemEffectEl);
+
+			this.compareWrapper.insertBefore(compareItemWrapper, this.compareWrapper.childNodes[0]);
+		}
+
+		setTimeout(function () {
+			// toggle compare basket
+			classie.remove(self.el, 'compare-basket--active');
+			// animate..
+			classie.add(viewEl, 'view--compare');
+		}, 25);
+	};
+
+	CompareBasket.prototype.isFull = function () {
+		return this.totalItems === this.itemsAllowed;
+	};
+
+	function init() {
+		// initialize an empty basket
+		basket = new CompareBasket();
+		initEvents();
+	}
+
+	function initEvents() {
+		items.forEach(function (item) {
+			var checkbox = item.querySelector('.action--compare-add > input[type = "checkbox"]');
+			checkbox.checked = false;
+
+			// ctrl to add to the "compare basket"
+			checkbox.addEventListener('click', function (ev) {
+				if (ev.target.checked) {
+					if (basket.isFull()) {
+						ev.preventDefault();
+						return false;
+					}
+					basket.add(item);
+				}
+				else {
+					basket.remove(item);
+				}
+			});
+		});
+	}
+
+
+
+	init();
+
+})();
